4655c52ef037464530cd594977eee7fc74fc1a72
Ola Ormset <ola-github@ormset.no>
2022-12-06 13:24:15 +0100

    Day 5 - not started

---
+++ 2022/05/task.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import sys
+
+
+
+for line in open(sys.argv[1]).readlines():
+    line = line.strip()
+    if not line:
+        continue
+
+  
+
+#print("Solution part one:")
+#print(overlaps)
+#print("Solution part two:")
+#print(partials)
+
+
+
