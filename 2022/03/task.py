923525856a82d44aa53da1b76f23ede84b700b03
Ola Ormset <ola-github@ormset.no>
2022-12-06 12:38:50 +0100

    Second solution day 3

--- 2022/03/task.py
+++ 2022/03/task.py
@@ -5,12 +5,29 @@ pri = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 common = ''
 
+badges = ''
+
 result = 0
+secresult = 0
+
+i = 0
+lines = []
 
 for line in open(sys.argv[1]).readlines():
     line = line.strip()
     if not line:
         continue
+
+    lines.append(line)
+    i += 1
+    if not i % 3:
+        for x in lines[0]:
+            if (x in lines[1]) and (x in lines[2]):
+                badges += x
+                break
+        i = 0
+        lines = []
+
 
     one = line[0:int(len(line)/2)]
     two = line[int(len(line)/2):]
@@ -23,12 +40,14 @@ for line in open(sys.argv[1]).readlines():
 for item in common:
     result += pri.index(item) + 1
 
+for badge in badges:
+    secresult += pri.index(badge) +1
 
 
 print("Solution part one:")
-print(common,result)
-#print("Solution part two:")
-#print(parttwopoints)
+print(result)
+print("Solution part two:")
+print(secresult)
 
 
 
